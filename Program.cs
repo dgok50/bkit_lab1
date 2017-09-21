5747db6bf2249298b296c70302a488fda5218af7
dgok50 <dgok50@db.a1mc.ru>
2017-09-21 18:47:50 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    { 
+ 
+            
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Введите первый коэффицент квадратного ур-я");
+                Program.try_read(out double a);
+
+                Console.WriteLine("Введите второй коэффицент квадратного ур-я");
+                Program.try_read(out double b);
+
+                Console.WriteLine("Введите третий коэффицент квадратного ур-я");
+                Program.try_read(out double c);
+
+                double x1, x2, D;
+
+                Console.WriteLine("Д = " + (D = b * b - 4 * a * c));
+
+                if (D > 0)
+                {
+                    x1 = ((-b - Math.Sqrt(D)) / 2 / a);
+                    x2 = ((-b + Math.Sqrt(D)) / 2 / a);
+                    Console.WriteLine("Корни уравнения: x1 =" + x1 + " x2 =" + x2); 
+                }
+                else if (D == 0)
+                {
+                    Console.WriteLine("Дискриминант равен 0, один корень x = " + (-b / 2 / a));
+                }
+                else
+                {
+                    Console.WriteLine("Корней нет, дискриминант меньше нуля");
+                }
+                Console.WriteLine("Хотите решить ещё одно уравние (да/нет):");
+            } while (Program.ans_to_bool() == true);
+        }
+
+        static bool ans_to_bool()
+        {
+            string enter = Console.ReadLine();
+            if (enter == "да" || enter == "д" || enter == "yes" || enter == "y")
+                return true;
+            else
+                return false;
+        }
+
+        static void try_read(out double tmp)
+        {
+            bool error = false;
+            tmp = -1;
+            do
+            {
+                string enter = Console.ReadLine();
+                try
+                {
+                    error = false;
+                    tmp = double.Parse(enter);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введен неверный коэффицент.\n Введите повторно: ");
+                    error = true;
+                }
+            }
+            while (error == true);
+        } 
+    }
+
+}
+
